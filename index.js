83c58b29b575fd60cb3e445abbad53b47e41b105
Zabdiel Perez <zabdielperez@Zabdiels-MacBook-Air.local>
2024-09-27 10:01:22 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+
+
+// function to handle submit
+function handleSubmit(event) {
+    event.preventDefault();
+
+    // Get pokemon value 
+    const pokemonInput = document.getElementById("pokemonInput").value
+    console.log("Fetching data for: ", pokemonInput);
+}
